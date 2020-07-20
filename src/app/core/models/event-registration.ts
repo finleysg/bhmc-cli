454c96f51dc860864ef91d1e4ea6d61c2c02abe4
Stuart Finley <finleysg@gmail.com>
2020-07-19 20:03:23 -0500

    temporary start time change

--- src/app/core/models/event-registration.ts
+++ src/app/core/models/event-registration.ts
@@ -49,26 +49,51 @@ export class EventRegistration {
     //     return `${this.holeNumber}${this.startingOrder === 0 ? 'A' : 'B' }`;
     // }
 
+    // get startingHoleName(): string {
+    //     switch (this.holeNumber) {
+    //         case 1:
+    //             return this.startingOrder === 0 ? '3:00' : '3:09';
+    //         case 2:
+    //             return this.startingOrder === 0 ? '3:18' : '3:27';
+    //         case 3:
+    //             return this.startingOrder === 0 ? '3:36' : '3:45';
+    //         case 4:
+    //             return this.startingOrder === 0 ? '3:54' : '4:03';
+    //         case 5:
+    //             return this.startingOrder === 0 ? '4:12' : '4:21';
+    //         case 6:
+    //             return this.startingOrder === 0 ? '4:30' : '4:39';
+    //         case 7:
+    //             return this.startingOrder === 0 ? '4:48' : '4:57';
+    //         case 8:
+    //             return this.startingOrder === 0 ? '5:06' : '5:15';
+    //         case 9:
+    //             return this.startingOrder === 0 ? '5:24' : '5:33';
+    //         default:
+    //             return '';
+    //     }
+    // }
+
     get startingHoleName(): string {
         switch (this.holeNumber) {
             case 1:
-                return this.startingOrder === 0 ? '3:00' : '3:09';
-            case 2:
-                return this.startingOrder === 0 ? '3:18' : '3:27';
-            case 3:
-                return this.startingOrder === 0 ? '3:36' : '3:45';
-            case 4:
-                return this.startingOrder === 0 ? '3:54' : '4:03';
-            case 5:
-                return this.startingOrder === 0 ? '4:12' : '4:21';
-            case 6:
                 return this.startingOrder === 0 ? '4:30' : '4:39';
-            case 7:
+            case 2:
                 return this.startingOrder === 0 ? '4:48' : '4:57';
-            case 8:
+            case 3:
                 return this.startingOrder === 0 ? '5:06' : '5:15';
-            case 9:
+            case 4:
                 return this.startingOrder === 0 ? '5:24' : '5:33';
+            case 5:
+                return this.startingOrder === 0 ? '5:42' : '5:51';
+            case 6:
+                return this.startingOrder === 0 ? '6:00' : '6:09';
+            case 7:
+                return this.startingOrder === 0 ? '6:18' : '6:27';
+            case 8:
+                return this.startingOrder === 0 ? '6:36' : '6:45';
+            case 9:
+                return this.startingOrder === 0 ? '6:54' : '7:03';
             default:
                 return '';
         }
